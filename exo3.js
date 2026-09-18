4e1a23ed945e76343c12506312258f082f363a9f
saviola24 <saviolafenitr@gmail.com>
2026-09-18 14:02:31 +0300

    add exo3.js in the project

---
+++ exo3.js
@@ -0,0 +1,20 @@
+import { Echanger, NombreUn } from './exo2.js';
+
+export function triLignes(matrice) {
+	for (let position = 0; position < matrice.length - 1; position++) {
+		let ligneAvecLeMoinsDeUn = position;
+
+		for (let ligne = position + 1; ligne < matrice.length; ligne++) {
+			if (NombreUn(matrice[ligne]) < NombreUn(matrice[ligneAvecLeMoinsDeUn])) {
+				ligneAvecLeMoinsDeUn = ligne;
+			}
+		}
+
+		if (ligneAvecLeMoinsDeUn !== position) {
+			Echanger(matrice, position, ligneAvecLeMoinsDeUn);
+		}
+	}
+
+	return matrice;
+}
+console.log(triLignes([[1, 0, 2], [0, 0, 3], [1, 1, 1]])); // [[0, 0, 3], [1, 0, 2], [1, 1, 1]]
