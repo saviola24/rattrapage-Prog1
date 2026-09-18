4cd3f38f9e2b1d0fa38030df3b561c1be9249aca
saviola24 <saviolafenitr@gmail.com>
2026-09-18 13:47:53 +0300

    add exo1.js in the project

---
+++ exo1.js
@@ -0,0 +1,23 @@
+
+function NegatifPositif(tableau) {
+  let gauche = 0;
+  let droite = tableau.length - 1;
+
+  while (gauche < droite) {
+    while (gauche < droite && tableau[gauche] <= 0) {
+      gauche++;
+    }
+
+    while (gauche < droite && tableau[droite] > 0) {
+      droite--;
+    }
+
+    if (gauche < droite) {
+      const temporaire = tableau[gauche];
+      tableau[gauche] = tableau[droite];
+      tableau[droite] = temporaire;
+    }
+  }
+
+  return tableau;
+}
